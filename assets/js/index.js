ce48d20912ead0e8f5febfe139e3348773ab5ccc
maxke24 <32979105+maxke24@users.noreply.github.com>
2020-09-29 20:11:48 +0200

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,28 @@
-"use strict";
+"use strict"
+document.addEventListener("DOMContentLoaded", init);
+
+let recipeList;
+let allRecipes = [];
+
+function init() {
+    recipeList = document.querySelector(".recipes");
+    loadRecipes();
+}
+
+function loadRecipes() {
+    fetch("assets/js/recipes.json")
+        .then(response => response.json())
+        .then(json => {
+            for(let recipe in json){
+                allRecipes.push(recipe);
+            }
+            fillRecipes();
+        });
+}
+
+function fillRecipes() {
+    for(let i = 0; i < 100; i++) {
+        recipeList.innerHTML += `<li><figure><img src="media/${allRecipes[i]}.jpg" alt="recipe"><figcaption>${allRecipes[i].replaceAll("-", " ")}</figcaption></figure></li>`;
+    }
+
+}
